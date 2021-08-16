2ce9b4c34912353e96dda7c295f0b0e03d8b7b16
yyd <695397065@qq.com>
2021-08-16 09:47:30 +0800

    finish malloc lab

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -25,7 +25,7 @@
 // Basic constants and macros
 #define WSIZE 4             // word and header/footer size (bytes)
 #define DSIZE 8             // double word size (bytes)
-#define CHUNKSIZE (1 << 12) //extend heap by this amount (Bytes)
+#define CHUNKSIZE 2056 //extend heap by this amount (Bytes)
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
@@ -41,13 +41,18 @@
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
 // given block ptr bp, compute address of its header and footer
-#define HDRP(bp) ((char *)(bp)-WSIZE)
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define HDRP(bp) ((char *)(bp)-3 * WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - 2 * DSIZE)
+
+#define PRED(bp) ((char *)(bp)-2 * WSIZE)
+#define SUCC(bp) ((char *)(bp)-1 * WSIZE)
 
 // given block ptr bp, compute address of next and previous blocks
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp)-DSIZE))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-2 * DSIZE))
 
+// segregated relate
+#define NUM_BLOCK 12
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
@@ -73,13 +78,18 @@ team_t team = {
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 // global val
-static char *heap_listp;
+static size_t list_num = 8;
+static size_t min_block = 5;
+static unsigned int *heap_listp;
 
 // helper function
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
 static void place(char *bp, size_t size);
 static void *find_fit(size_t size);
+static void *find_list(size_t size);
+static void insert_node(void *bp, size_t size);
+static void delete_node(void *bp);
 // static void check(void);
 
 /* 
@@ -88,13 +98,16 @@ static void *find_fit(size_t size);
 int mm_init(void)
 {
     // create the initial empty heap
-    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+    if ((heap_listp = mem_sbrk((4 + list_num) * WSIZE)) == (void *)-1)
         return -1;
-    PUT(heap_listp, 0);                            // alighment padding
-    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); // prologue header
-    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); // prologue footer
-    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));     // epilogue header
-    heap_listp += 2 * (WSIZE);
+    for (int i = 0; i < list_num; i++)
+    {
+        PUT(heap_listp + i, 0); // header of lists
+    }
+    PUT(heap_listp + (list_num + 0), 0);              // for aligment
+    PUT(heap_listp + (list_num + 1), PACK(DSIZE, 1)); // prologue header
+    PUT(heap_listp + (list_num + 2), PACK(DSIZE, 1)); // prologue footer
+    PUT(heap_listp + (list_num + 3), PACK(0, 1));     // epilogue header
 
     // extend the empty heap with a free block of CHUNKSIZE bytes
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
@@ -109,9 +122,9 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    size_t newsize = ALIGN(size +DSIZE);
+    size_t newsize = ALIGN(size + 2 * DSIZE);
     size_t extendszie;
-    char *bp;
+    unsigned *bp;
 
     // ignore spurious requets
     if (size == 0)
@@ -123,13 +136,14 @@ void *mm_malloc(size_t size)
         place(bp, newsize);
         return bp;
     }
- 
+
     // if not find fit. get more memory and place the block
     extendszie = MAX(newsize, CHUNKSIZE);
 
-    if ((bp = extend_heap(extendszie/WSIZE)) == NULL)
+    if ((bp = extend_heap(extendszie / WSIZE)) == NULL)
         return NULL;
 
+    delete_node(bp);
     place(bp, newsize);
     return bp;
 }
@@ -168,14 +182,15 @@ void *mm_realloc(void *ptr, size_t size)
 
 static void *extend_heap(size_t words)
 {
-    char *bp;
+    unsigned *bp;
     size_t size;
 
     // change size to maintain alignment
-    size = (words % 2)? (words+1)*WSIZE : words* WSIZE;
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
     if ((long)(bp = mem_sbrk(size)) == -1)
         return NULL;
 
+    bp += 2;
     // initialize free block header and footer and epilogue header
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
@@ -187,18 +202,19 @@ static void *extend_heap(size_t words)
 
 void *coalesce(void *bp)
 {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
     // case 1
     if (prev_alloc && next_alloc)
-        return bp;
-
+    {
+    }
     // case 2
-    if (prev_alloc && !next_alloc)
+    else if (prev_alloc && !next_alloc)
     {
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        delete_node(NEXT_BLKP(bp));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
     }
@@ -206,6 +222,7 @@ void *coalesce(void *bp)
     else if (!prev_alloc && next_alloc)
     {
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        delete_node(PREV_BLKP(bp));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
@@ -213,22 +230,39 @@ void *coalesce(void *bp)
     else // case 4
     {
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        delete_node(NEXT_BLKP(bp));
+        delete_node(PREV_BLKP(bp));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
+
+    insert_node(bp, size);
     return bp;
 }
 
 void *find_fit(size_t size)
 {
-    char *bp = heap_listp;
+    unsigned int *head = find_list(size);
+    unsigned int *bp;
+    // if no node in the list
+    if (head == NULL)
+        return NULL;
 
-    while (GET_SIZE(HDRP(bp)) > 0)
+    while (head < heap_listp + list_num)
     {
-        if (GET_ALLOC(HDRP(bp)) == 0 && GET_SIZE(HDRP(bp)) > size)
-            return bp;
-        bp=NEXT_BLKP(bp);
+        bp = GET(head);
+
+        while (bp != NULL)
+        {
+            if (GET_SIZE(HDRP(bp)) >= size)
+            {
+                delete_node(bp);
+                return bp;
+            }
+            bp = GET(SUCC(bp));
+        }
+        head++;
     }
 
     return NULL;
@@ -237,18 +271,64 @@ void *find_fit(size_t size)
 void place(char *bp, size_t asize)
 {
     size_t csize = GET_SIZE(HDRP(bp));
-
-    if (csize - asize >= 2 * DSIZE) 
+    size_t msize = 1 << min_block;
+    if (csize - asize >= msize)
     {
         PUT(HDRP(bp), PACK(asize, 1));
         PUT(FTRP(bp), PACK(asize, 1));
         bp = NEXT_BLKP(bp);
-        PUT(HDRP(bp), PACK(csize - asize, 1));
-        PUT(FTRP(bp), PACK(csize - asize, 1));
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+        insert_node(bp, csize - asize);
     }
     else
     {
         PUT(HDRP(bp), PACK(csize, 1));
         PUT(FTRP(bp), PACK(csize, 1));
     }
+}
+
+// return ptr of list based on the size
+static void *find_list(size_t size)
+{
+    size_t bits = 32 - __builtin_clz(size);
+
+    if (bits <= min_block)
+        return heap_listp;
+
+    if (bits >= min_block + list_num)
+        return heap_listp + (list_num - 1);
+
+    return heap_listp + (bits - (min_block + 1));
+}
+
+void insert_node(void *bp, size_t size)
+{
+    unsigned *head = find_list(size);
+    unsigned *old = GET(head);
+
+    if (old != NULL)
+        PUT(PRED(old), bp);
+    PUT(SUCC(bp), old);
+    PUT(PRED(bp), head);
+    PUT(head, bp);
+}
+void delete_node(void *bp)
+{
+    unsigned *pre = GET(PRED(bp));
+    unsigned *suc = GET(SUCC(bp));
+
+    // if pre is head
+    if (pre < heap_listp + list_num)
+    {
+        PUT(pre, suc);
+    }
+    else
+    {
+        PUT(SUCC(pre), suc);
+    }
+
+    // if suc is NULL
+    if (suc != 0)
+        PUT(PRED(suc), pre);
 }
